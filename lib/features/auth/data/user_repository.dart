6f0d47e7fb0e3f49ca989125864eaee18e389a5b
Muskan Poddar <poddarmuskan888@gmail.com>
2025-12-11 19:47:29 +0530

    Modified user_repository file

--- lib/features/auth/data/user_repository.dart
+++ lib/features/auth/data/user_repository.dart
@@ -2,15 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../domain/models/user_model.dart';
 
 class UserRepository {
-  final users = FirebaseFirestore.instance.collection('users');
+  final CollectionReference users = FirebaseFirestore.instance.collection(
+    'users',
+  );
 
   Future<void> createUser(AppUser user) async {
-    await users.doc(user.uid).set(user.toMap());
+    await users.doc(user.uid).set(user.toMap(), SetOptions(merge: true));
   }
 
   Future<AppUser?> getUser(String uid) async {
     final doc = await users.doc(uid).get();
     if (!doc.exists) return null;
-    return AppUser.fromMap(doc.data()!);
+    return AppUser.fromMap(doc.data()! as Map<String, dynamic>);
   }
 }

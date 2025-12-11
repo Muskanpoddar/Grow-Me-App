befe4a74aa1f4694e029ee1ea6efe0a3cdcf190c
Muskan Poddar <poddarmuskan888@gmail.com>
2025-12-11 19:51:59 +0530

    Modified home screen file

--- lib/features/auth/presentation/home_screen.dart
+++ lib/features/auth/presentation/home_screen.dart
@@ -6,8 +6,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: const Center(child: Text("Logged in")),
+      body: Center(
+        child: Text("You are logged in!", style: TextStyle(fontSize: 24)),
+      ),
     );
   }
 }

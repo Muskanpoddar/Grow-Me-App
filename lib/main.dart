4e54a278c2f2cca5e284e7c151eae49ac6f9ddbc
Muskan Poddar <poddarmuskan888@gmail.com>
2025-12-13 23:25:47 +0530

    Intialized file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:growme/app/auth_state_widget.dart';
+import 'package:growme/features/auth/presentation/comment_screen.dart';
+import 'package:growme/features/auth/presentation/create_post_screen.dart';
+import 'package:growme/features/auth/presentation/goal_Screen.dart';
+import 'package:growme/features/auth/presentation/goal_summary_screen.dart';
+import 'package:growme/features/auth/presentation/home_screen.dart';
+import 'package:growme/features/auth/presentation/updated_goal_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(const MyApp());
 }
 
@@ -17,8 +21,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'GrowMe',
       debugShowCheckedModeBanner: false,
-      home: AuthStateWidget(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xffF5F8F5),
+        fontFamily: 'Inter',
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: const Color(0xff00CC66),
+        ),
+      ),
+      home: const AuthStateWidget(),
+      routes: {
+        '/home': (_) => const HomeScreen(),
+        '/createPost': (_) => const CreatePostScreen(),
+        '/comments': (_) => const CommentsScreen(),
+        '/createGoal': (_) => const CreateGoalScreen(),
+        '/goalSummary': (_) => const GoalSummaryScreen(),
+        '/goals': (_) => GoalsScreen(),
+      },
     );
   }
 }

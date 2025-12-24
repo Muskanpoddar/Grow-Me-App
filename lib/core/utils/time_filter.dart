a891647fe5cbba973463a324e1d73888ef0c8dcf
Muskan Poddar <poddarmuskan888@gmail.com>
2025-12-24 22:54:46 +0530

    Intialize new file

---
+++ lib/core/utils/time_filter.dart
@@ -0,0 +1,15 @@
+enum TimeFilter { week, month, year, all }
+
+DateTime getStartDate(TimeFilter filter) {
+  final now = DateTime.now();
+  switch (filter) {
+    case TimeFilter.week:
+      return now.subtract(const Duration(days: 7));
+    case TimeFilter.month:
+      return DateTime(now.year, now.month - 1, now.day);
+    case TimeFilter.year:
+      return DateTime(now.year - 1, now.month, now.day);
+    case TimeFilter.all:
+      return DateTime(2000);
+  }
+}

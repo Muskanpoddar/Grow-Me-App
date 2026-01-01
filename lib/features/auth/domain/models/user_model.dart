1f4e99d96fcba14b8a9216d6845368bd05b52632
Muskan Poddar <poddarmuskan888@gmail.com>
2026-01-01 21:56:44 +0545

    Modified files

--- lib/features/auth/domain/models/user_model.dart
+++ lib/features/auth/domain/models/user_model.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class AppUser {
   final String uid;
   final String name;
   final String email;
   final String username;
+  final String usernameLower;
   final String? photoUrl;
   final List<String> interests;
 
@@ -11,6 +14,7 @@ class AppUser {
     required this.name,
     required this.email,
     required this.username,
+    required this.usernameLower,
     required this.interests,
     this.photoUrl,
   });
@@ -19,21 +23,25 @@ class AppUser {
     return {
       'uid': uid,
       'name': name,
+      'name_lower': name.toLowerCase(),
       'email': email,
       'username': username,
-      'interests': interests,
+      'username_lower': usernameLower,
       'photoUrl': photoUrl,
+      'interests': interests,
+      'createdAt': FieldValue.serverTimestamp(),
     };
   }
 
   factory AppUser.fromMap(Map<String, dynamic> map) {
     return AppUser(
       uid: map['uid'],
-      name: map['name'],
-      email: map['email'],
-      username: map['username'],
-      interests: List<String>.from(map['interests']),
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      username: map['username'] ?? '',
+      usernameLower: map['username_lower'] ?? '',
       photoUrl: map['photoUrl'],
+      interests: List<String>.from(map['interests'] ?? []),
     );
   }
 }
